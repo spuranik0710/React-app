442bc6b654c66da961b15ce611fff17cb9c13bee
Shruti Puranik <spuranik@hawk.iit.edu>
2018-02-20 16:46:10 -0600

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './App'
 import './index.css';
 import 'github-fork-ribbon-css/gh-fork-ribbon.css';
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.getElementById('root')
 );
